281a81427ede450dc72acf973643a9a04335953d
Hanif Maliki Dewanto <hanif.dewanto@alterra.id>
2024-12-28 04:12:22 +0700

    Menu expand

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,16 +1,14 @@
-"use client"
+"use client";
 
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 function Sidebar ({ children }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isEcommerceMenuOpen, setIsEcommerceMenuOpen] = useState(false);
 
-  // Effect to set the initial state based on screen size
   useEffect(() => {
     const checkScreenSize = () => {
-      // On mobile screens (max-width: 768px), the sidebar is closed by default
-      // On larger screens, the sidebar is open by default
       if (window.innerWidth >= 768) {
         setIsSidebarOpen(true);
       } else {
@@ -18,13 +16,9 @@ function Sidebar ({ children }) {
       }
     };
 
-    // Check screen size on initial load
     checkScreenSize();
-
-    // Add event listener for window resize
     window.addEventListener('resize', checkScreenSize);
 
-    // Cleanup the event listener on component unmount
     return () => window.removeEventListener('resize', checkScreenSize);
   }, []);
 
@@ -32,18 +26,18 @@ function Sidebar ({ children }) {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
+  const toggleEcommerceMenu = () => {
+    setIsEcommerceMenuOpen(!isEcommerceMenuOpen);
+  };
+
   return (
     <>
       <button
         onClick={toggleSidebar}
-        data-drawer-target="sidebar-multi-level-sidebar"
-        data-drawer-toggle="sidebar-multi-level-sidebar"
-        aria-controls="sidebar-multi-level-sidebar"
-        type="button"
         className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 sm:absolute"
       >
         <span className="sr-only">Open sidebar</span>
-        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
         </svg>
       </button>
@@ -53,14 +47,9 @@ function Sidebar ({ children }) {
         className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50" style={{ backgroundColor: '#0f1827' }}>
+        <div className="h-full px-3 py-4 overflow-y-auto" style={{ backgroundColor: '#0f1827' }}>
           <a href="/" className="flex items-center ps-2.5 mb-5">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              width={100}
-              height={50}
-            />
+            <Image src="/logo.png" alt="Logo" width={100} height={50} />
           </a>
           <button
             onClick={toggleSidebar}
@@ -71,16 +60,10 @@ function Sidebar ({ children }) {
               <path d="M6 6L14 14M6 14L14 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
             </svg>
           </button>
-          <ul className="space-y-2 font-medium">
+          <ul className="space-y-2 font-medium text-white">
             <li>
-              <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100">
-                <svg
-                  className="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 21"
-                >
+              <a href="#" className="flex items-center p-2 rounded-lg hover:bg-gray-700">
+                <svg className="w-5 h-5 text-gray-300 transition duration-75 group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                 </svg>
@@ -90,55 +73,36 @@ function Sidebar ({ children }) {
             <li>
               <button
                 type="button"
-                className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100"
-                aria-controls="dropdown-example"
-                data-collapse-toggle="dropdown-example"
+                onClick={toggleEcommerceMenu}
+                className="flex items-center w-full p-2 text-white transition duration-75 rounded-lg hover:bg-gray-700"
               >
-                <svg
-                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 18 21"
-                >
-                  <path
-                    d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"
-                  />
+                <svg className="w-5 h-5 text-gray-300 group-hover:text-white" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
+                  <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
                 </svg>
-                <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">E-commerce</span>
-                <svg
-                  className="w-3 h-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 1 4 4 4-4"
-                  />
+                <span className="flex-1 ms-3 text-left">E-commerce</span>
+                <svg className="w-3 h-3" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 6">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                 </svg>
               </button>
-              <ul id="dropdown-example" className="hidden py-2 space-y-2">
-                <li>
-                  <a href="#" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100">
-                    Products
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100">
-                    Billing
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100">
-                    Invoice
-                  </a>
-                </li>
-              </ul>
+              {isEcommerceMenuOpen && (
+                <ul className="py-2 space-y-2 bg-gray-700">
+                  <li>
+                    <a href="#" className="flex items-center p-2 pl-11 text-white rounded-lg hover:bg-gray-600">
+                      Products
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="flex items-center p-2 pl-11 text-white rounded-lg hover:bg-gray-600">
+                      Billing
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="flex items-center p-2 pl-11 text-white rounded-lg hover:bg-gray-600">
+                      Invoice
+                    </a>
+                  </li>
+                </ul>
+              )}
             </li>
           </ul>
         </div>
