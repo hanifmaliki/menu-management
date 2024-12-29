527bd34d3926efef89e40428d4b56fadda3862ac
Hanif Maliki Dewanto <hanif.dewanto@alterra.id>
2024-12-29 07:24:33 +0700

    Try fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  // Enable CORS
-  app.enableCors({
-    origin: '*', // Allow specific origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // If you need to allow cookies
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 8080);
 }
